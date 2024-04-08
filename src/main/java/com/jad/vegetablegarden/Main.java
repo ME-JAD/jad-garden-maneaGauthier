e68f7342e3ce714af557cdf107aea5d47606c31b
maneaGauthier <118429687+maneaGauthier@users.noreply.github.com>
2024-04-08 09:39:12 +0200

    yo

--- src/main/java/com/jad/vegetablegarden/Main.java
+++ src/main/java/com/jad/vegetablegarden/Main.java
@@ -2,6 +2,8 @@ package com.jad.vegetablegarden;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsoleWindow window = new ConsoleWindow("Le jardin de Jad");
+        window.setScreen("hello");
+        window.repaint();
     }
 }
---
+++ src/main/java/com/jad/vegetablegarden/vegetable/Vegetable.java
@@ -0,0 +1,70 @@
+package com.jad.vegetablegarden.vegetable;
+
+public class Vegetable {
+    private int maxAge;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private int age;
+    private String name;
+    private int growingRange;
+    private boolean alive;
+    private SpriteCell spriteCell;
+
+    protected void die() {
+        this.alive = false;
+    }
+
+    protected void grow() {
+        this.age++;
+        if (this.age >= this.maxAge) {
+            this.die();
+        }
+    }
+
+    public Vegetable(SpriteCell firstSprite, String name, int maxAge, int growingRange) {
+        this.spriteCell = firstSprite;
+        this.name = name;
+        this.maxAge = maxAge;
+        this.growingRange = growingRange;
+        this.age = 0;
+        this.alive = true;
+    }
+
+    private void manageSprite() {
+        if (this.age < this.growingRange) {
+            this.spriteCell = this.spriteCell.getNext();
+        }
+    }
+
+    protected void goNextSprite() {
+        this.spriteCell = this.spriteCell.getNext();
+    }
+
+    public String getLine(int line) {
+        return this.spriteCell.getLine(line);
+    }
+
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Vegetable{" +
+                "maxAge=" + maxAge +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", growingRange=" + growingRange +
+                ", alive=" + alive +
+                ", spriteCell=" + spriteCell +
+                '}';
+    }
+}
